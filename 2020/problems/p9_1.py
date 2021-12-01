9a1b2f53666a0ddaa10dc11e6a8e5e4613c7aef3
Matt Boyle <matt.boyle@datadoghq.com>
2021-12-01 10:57:03 -0500

    Sure, all of it

--- 2020/problems/p9_1.py
+++ 2020/problems/p9_1.py
@@ -1,16 +1,15 @@
-input = open('../data/p1_data.txt', 'r')
+input = open('../data/p9_test_data.txt', 'r')
+preamble_len = 5
 
-data = input.readlines()
-for a in data:
-    a = int(a.strip())
-    for b in data:
-        b = int(b.strip())
-        if a == b:
-            continue
-        if a + b == 2020:
-            print(a * b)
-            exit()
+def check_for_sums(sum, window):
+    for x in window:
 
 
 
 
+xmas = input.read().splitlines()
+xmas = [int(x) for x in xmas]
+preamble = xmas[:preamble_len]
+xmas = xmas[preamble:]
+
+for x in xmas:
