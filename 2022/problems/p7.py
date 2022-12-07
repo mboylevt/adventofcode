5aaeb3c66b675c084cc34898c073a45b41601c9a
mattboyle <matt.boyle@datadoghq.com>
2022-12-07 10:19:15 -0500

    P7

---
+++ 2022/problems/p7.py
@@ -0,0 +1,33 @@
+input = open('../data/p7_data.txt', 'r')
+# input = open('../data/p7_test_data.txt', 'r')
+lines = input.read().splitlines()
+
+def parse_input(lines):
+    stack = [{}]
+    for line in lines:
+        match line.split():
+            case ['$', 'cd', '/']:  stack = [stack[0]]
+            case ['$', 'cd', '..']: stack.pop()
+            case ['$', 'cd', dir]:  stack.append(stack[-1][dir])
+            case ['$', 'ls']:       continue
+            case ['dir', dir]:      stack[-1][dir] = {}
+            case [size, fname]:     stack[-1][fname] = int(size)
+    return stack
+
+stack = parse_input(lines)
+sizes = []
+
+
+def get_total_sizes(file):
+
+    if isinstance(file, int):
+        return file
+    total = sum(get_total_sizes(d) for d in file.values())
+    sizes.append(total)
+    return total
+
+
+ts = get_total_sizes(stack[0]) - 40000000
+print(sum(size for size in sizes if size <= 100000 ))
+print(min((size for size in sizes if size >= ts )))
+i = 1
