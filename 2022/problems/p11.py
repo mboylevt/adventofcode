7f840b484250815d6b30e28d0ee32e45a822429f
mattboyle <matt.boyle@datadoghq.com>
2022-12-11 11:35:40 -0500

    LCM is your friend

--- 2022/problems/p11.py
+++ 2022/problems/p11.py
@@ -1,6 +1,6 @@
 input = open('../data/p11_data.txt', 'r')
 # input = open('../data/p11_test_data.txt', 'r')
-
+from math import gcd
 import re
 
 class Monkey():
@@ -47,29 +47,48 @@ class Monkey():
     def test(self, item):
         return True if item % self.test_div == 0 else False
 
-    def take_turn(self, monkeys):
+    def take_turn(self, monkeys, LCM, worry_reduction):
         while True:
             if len(self.items) == 0: break
             self.inspection_count += 1
             item = self.items.pop(0)
             output = self.operate(item)
-            output = int(output/3)
+            if worry_reduction:
+                output = int(output/3)
+            else:
+                output = output % LCM
             if self.test(output):
                 monkeys[self.true_monkey].items.append(output)
             else:
                 monkeys[self.false_monkey].items.append(output)
 
 
+def lcm(lst):
+    lcm = 1
+    for i in lst:
+        lcm = lcm * i // gcd(lcm, i)
+    return lcm
 
+def monkey_business(input_monkeys, worry_reduction, rounds):
 
+    monkeys = [Monkey(monkey_input) for monkey_input in input_monkeys]
+    monkeys = {monkey.monkey_id: monkey for monkey in monkeys}
+
+    divisors = []
+    for index, monkey in monkeys.items():
+        divisors.append(monkey.test_div)
+    LCM = lcm(divisors)
+
+    for round in range(0, rounds):
+        for monkey_id in monkeys.keys():
+            monkeys[monkey_id].take_turn(monkeys, LCM, worry_reduction)
+
+    inspections = sorted([monkey.inspection_count for monkey in monkeys.values()], reverse=True)
+    return inspections
 
 input_monkeys = [line for line in input.read().split('\n\n')]
-monkeys = [Monkey(monkey_input) for monkey_input in input_monkeys]
-monkeys = {monkey.monkey_id: monkey for monkey in monkeys}
-
-for round in range(0, 20):
-    for monkey_id in monkeys.keys():
-        monkeys[monkey_id].take_turn(monkeys)
-
-inspections = sorted([monkey.inspection_count for monkey in monkeys.values()], reverse=True)
+input_monkeys_p2 = input_monkeys.copy()
+inspections = monkey_business(input_monkeys, True, 20)
 print("Part 1: {}".format(inspections[0] * inspections[1]))
+inspections = monkey_business(input_monkeys_p2, False, 10000)
+print("Part 2: {}".format(inspections[0] * inspections[1]))
