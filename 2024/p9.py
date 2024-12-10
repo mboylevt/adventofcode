e96868272a8ae7f4497f6f168275fdc6fc0f2b35
matt boyle <matthew.p.boyle@gmaill.com>
2024-12-10 00:05:03 -0500

    p9

--- 2024/p9.py
+++ 2024/p9.py
@@ -45,4 +45,72 @@ class Block:
     length: int
 
 fsblocks = []
-for i in range(len(disk_start)):
+i = 0
+while i < len(disk_start):
+    test_char = cur_char = disk_start[i]
+    cursor = i
+    while cur_char == test_char:
+        cursor += 1
+        if cursor == len(disk_start):
+            break
+        test_char = disk_start[cursor]
+    fsblocks.append(Block(cur_char, cursor-i))
+    i = cursor
+
+# print(f'Disk layout: ' + ''.join(disk_start))
+
+compressed_blocks = fsblocks.copy()
+reversed_blocks = compressed_blocks.copy()
+reversed_blocks.reverse()
+
+# For all the blocks in reverse order
+for moveBlock in reversed_blocks:
+    if moveBlock.content == '.':
+        continue
+    moveBlockIndex = compressed_blocks.index(moveBlock)
+    for targetBlockIndex in range(0, len(compressed_blocks)):
+        targetBlock = compressed_blocks[targetBlockIndex]
+
+        # If this isn't a free space block (or to the left of the move), then ignore it
+        if targetBlock.content != '.' or targetBlockIndex >= moveBlockIndex:
+            continue
+
+        # If this is a free space block AND it's large enough to accommodate the move block, move it
+        if targetBlock.length >= moveBlock.length:
+            resize = False
+            # If the target block is bigger than the move block, we need to resize the target block
+            # to account for the remaining space
+            if targetBlock.length != moveBlock.length:
+                targetBlock.length = targetBlock.length - moveBlock.length
+                resize = True
+
+            # Remove the old blocks, insert the new ones
+            # only add the free space block back if there is space remaining
+            del compressed_blocks[moveBlockIndex]
+            del compressed_blocks[targetBlockIndex]
+            compressed_blocks.insert(targetBlockIndex, moveBlock)
+            compressed_blocks.insert(moveBlockIndex, Block('.', moveBlock.length))
+            if resize:
+                compressed_blocks.insert(targetBlockIndex+1, targetBlock)
+
+            # Do a pass to combine any adjacent .'s
+            newlen = len(compressed_blocks)
+            i = 0
+            while i < newlen-1:
+                if compressed_blocks[i].content == compressed_blocks[i+1].content:
+                    compressed_blocks[i].length += compressed_blocks[i+1].length
+                    del compressed_blocks[i+1]
+                    newlen = newlen - 1
+                    i = i - 1
+                i += 1
+            break
+
+fsmap = ''
+for block in compressed_blocks:
+    fsmap += str(block.content * block.length)
+
+checksum = 0
+for i in range(0, len(fsmap)):
+    if fsmap[i] != '.':
+        checksum += int(fsmap[i]) * i
+print(f'Part 2: {checksum}')
